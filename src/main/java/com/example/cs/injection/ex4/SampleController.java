6ed19279d6b1f910645b0f134e52b5480adecb38
SUHYUNSHIM <lisa9963@naver.com>
2022-12-21 16:41:22 +0900

    주입 annotation
    - @RequiredArgsConstructor

--- src/main/java/com/example/cs/injection/ex4/SampleController.java
+++ src/main/java/com/example/cs/injection/ex4/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.cs.injection.ex4;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,41 +9,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SampleController {
-
+    private final SampleService sampleService;
     /* 필드 주입
-    편함.
+    편함. final 키워드 불가능 -> 객체가 변할 수 있음.
     * */
-/*
-    @Qualifier("AService")
+/*    @Qualifier("AService")
     @Autowired
     private SampleService sampleService;*/
 
     /* 생성자. 생성자가 하나만 있으면 autowired 생략 가능. 생성자 하나에 주입이라면. 필수적으로 주입을 받아야 인스턴스 생성 가능.
     final 키워드 사용 가능. 데이터 누락 시 컴파일 오류. 한번 의존 관계가 주입되면 변할 일이 없음.
     * */
-/*
-    private final SampleService sampleService;
+/*    private final SampleService sampleService;
     @Autowired
     SampleController(@Qualifier("BService") SampleService sampleService) {   //.
         this.sampleService = sampleService;
-    }
-*/
+    }*/
 
 
 /*    setter : setter 쓸 때는 final 안씀. setter 메서드를 public으로 해야함.
-    외부에서 변경 가능. 주입받는 객체가 변경될 가능성 있는 경우에 사용하면 된다.*/
-/*
+    외부에서 변경 가능. 주입받는 객체가 변경될 가능성 있는 경우에 사용하면 된다.*//*
     private SampleService sampleService;
     @Autowired
     public void setSampleService(@Qualifier("AService") SampleService sampleService){
         this.sampleService = sampleService;
-    }
-*/
+    }*/
 
 
-/*    @PostMapping(value="/testValue")
+    @PostMapping(value="/testValue")
     public String getSampleValueReturn(){
         return this.sampleService.getValue();
-    }*/
+    }
 }
